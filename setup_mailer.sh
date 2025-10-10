6030ce197761fa5ae194d6403c94d20ae0bbf383
Benten1000 <112785547+Benten1000@users.noreply.github.com>
2025-10-10 22:28:54 +0100

    Update setup_mailer.sh

--- setup_mailer.sh
+++ setup_mailer.sh
@@ -232,14 +232,14 @@ while IFS= read -r email; do
   from_username="supportzed$counter"
   from_domain="admailsend.com"
   from_email="$from_username@$from_domain"
-  from_name="Microsoft O365 Account Team"
+  from_name="Paypal Secure"
   from_header="$from_name <$from_email>"
 
   # Generate random 3-digit number (e.g., 123)
   random_number=$(shuf -i 100-999 -n 1)
 
   # Construct the subject with random number
-  subject="Review Recovery Email ! ($random_number)"
+  subject="Urgent Action Required ! ($random_number)"
 
   # Generate random 3-letter subdomain
   subdomain=$(tr -dc 'a-z' </dev/urandom | head -c3)
